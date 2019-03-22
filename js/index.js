77eddf4feebc5691d8541d6bc0d6d4811dc9f02f
Juliana Mota <julianamota89@gmail.com>
2019-03-22 22:55:26 +0100

    homepage mobile working

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const titlePS = document.querySelectorAll(".st2");
+// titlePS.forEach(drawLetters);
+
+// function drawLetters(item, index) {
+//   setTimeout(function() {
+//     const l = item.getTotalLength();
+//     item.style.strokeDasharray = l;
+//     item.style.strokeDashoffset = l;
+//     item.classList.add("draw");
+//   }, index * 300);
+// }
