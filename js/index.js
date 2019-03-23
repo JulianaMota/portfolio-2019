21ca5598323654c9438bcb3724f96ecccd315a41
Juliana Mota <julianamota89@gmail.com>
2019-03-23 19:56:00 +0100

    projets js and style working

--- js/index.js
+++ js/index.js
@@ -1,11 +1,48 @@
-// const titlePS = document.querySelectorAll(".st2");
-// titlePS.forEach(drawLetters);
+"use strict";
 
-// function drawLetters(item, index) {
-//   setTimeout(function() {
-//     const l = item.getTotalLength();
-//     item.style.strokeDasharray = l;
-//     item.style.strokeDashoffset = l;
-//     item.classList.add("draw");
-//   }, index * 300);
-// }
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  get();
+}
+
+function get() {
+  fetch("https://restju-f026.restdb.io/rest/portfolioprojects?year=2018", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5c9667bddf5d634f46ecae24",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(e => e.json())
+    .then(e => {
+      console.log(e);
+      e.forEach(displayP2018);
+    });
+}
+
+function displayP2018(project) {
+  console.log(project);
+  const copy = document.querySelector("template").content.cloneNode(true);
+
+  copy.querySelector("img").src =
+    "https://restju-f026.restdb.io/media/" +
+    project.image +
+    "?key=5c9667bddf5d634f46ecae24";
+  copy.querySelector("h2").textContent = project.title;
+  copy.querySelector(".place").textContent = project.place;
+  const role = copy.querySelector(".role");
+  window.addEventListener("resize", function() {
+    if (window.matchMedia("(max-width: 700px)").matches) {
+      //   console.log("less");
+      role.style.display = "none";
+    } else {
+      //   console.log("more");
+      role.style.display = "block";
+      role.textContent = project.role;
+    }
+  });
+
+  document.querySelector("#p2018").appendChild(copy);
+}
